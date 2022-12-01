f016e2feb7f7516152c9572dde55acaaf334b43b
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-12-01 05:03:54 -0800

    2game files

---
+++ DigitalYouth2022-main/DigitalYouth2022-main/DigitalYouth-main/New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+		Speech.AddDialogue("0","Hello traveller!", "1");
+		Speech.AddDialogue("1", "Welcome to our planet!", "2");
+		Speech.AddDialogue("2", "Find powerup and you can jump higher", "3");
+		Speech.AddDialogue("3", "then go to next island");
+	}
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other ){
+
+		Speech.SetDialogue("0");
+}
+}
---
+++ Fantastic obby/Assets/mainscript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score;
+    public AudioSource coinmusic;
+    public Text scoreText;
+    public GameObject winText;
+    void Start()
+    {
+     scoreText.text = "Score: " + score;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "coin")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = "Score: " + score;
+            //coinmusic.Play();
+
+            if (collision.gameObject.tag == "danger")
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "finish")
+        {
+            winText.SetActive(true);
+        }
+    }
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
